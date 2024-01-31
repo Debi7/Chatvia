4257b63dcf04fcef71387c436949786f3234760f
Debi7 <debi1@mail.ru>
2024-01-31 03:16:58 +0300

    status

--- src/shared/LogoBlack/LogoBlack.jsx
+++ src/shared/LogoBlack/LogoBlack.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-<<<<<<< HEAD
 import styles from "../../pages/SignIn/SignIn.module.css";
 import logoSrc from "../../assets/icons/logoBlack.png";
 
@@ -13,21 +12,6 @@ const LogoBlack = () => (
     />
   </a>
 )
-=======
-import styles from "./LogoBlack.module.css";
-
-const LogoBlack = () => (
-  <>
-    <a>
-      <img
-        className={styles.logo}
-        src="../src/assets/icons/logoBlack.png"
-        alt="logo"
-      />
-    </a>
-  </>
-);
->>>>>>> change
 
 export default LogoBlack;
 
