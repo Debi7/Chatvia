7e99a3b0ea099d2170b18ceaa1130fa00a44956e
Debi7 <debi1@mail.ru>
2024-08-16 23:06:25 +0300

    predeploy

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,9 +1,9 @@
-const BASE_URL = process.env.GH_PAGES_BASE_URL ?? ""
+const BASE_URL = process.env.GH_PAGES_BASE_URL ?? "/"
 
 export const URLS = {
-  root: `${BASE_URL}#/`,
-  register: `${BASE_URL}#/register`,
-  reset: `${BASE_URL}#/reset`,
-  signin: `${BASE_URL}#/signin`,
-  chat: `${BASE_URL}#/chat`,
+  root: `${BASE_URL}`,
+  register: `${BASE_URL}register`,
+  reset: `${BASE_URL}reset`,
+  signin: `${BASE_URL}signin`,
+  chat: `${BASE_URL}chat`,
 }
